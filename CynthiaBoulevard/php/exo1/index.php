b408f4888d5badb2d5d82ddb96aca55f7be91afe
CynthiaBoulevard <cynthiaboulevard@gmail.com>
2019-12-02 14:16:08 -0300

    ex1

---
+++ CynthiaBoulevard/php/exo1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exo1</title>
+</head>
+<body>
+
+    <?php
+    
+    echo  
+      $nom = 'cynthia';    // Type string (chaine de caractères)
+    ?>
+
+</body>
+</html>
