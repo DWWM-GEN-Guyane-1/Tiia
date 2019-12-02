76159bb7b4ae017e110318844138f3065125b061
CynthiaBoulevard <cynthiaboulevard@gmail.com>
2019-12-02 14:22:21 -0300

    ex2

---
+++ CynthiaBoulevard/php/exo2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exo2</title>
+</head>
+<body>
+<?php
+  $nom = 'BOULEVARD';
+  $prenom = 'CYNTHIA';
+  $age = 24;
+ 
+  echo $nom;         
+  echo '<br/>';
+
+  echo $prenom;        
+  echo '<br/>';
+  echo $age;        
+ 
+ 
+ 
+?>
+</body>
+</html>
