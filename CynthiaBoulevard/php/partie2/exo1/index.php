4e8d11d4c30fd96b398363e2c6584c13ecc44b09
CynthiaBoulevard <cynthiaboulevard@gmail.com>
2019-12-03 21:57:20 -0300

    partie2

---
+++ CynthiaBoulevard/php/partie2/exo1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exo1</title>
+</head>
+<body>
+<?php
+
+
+$age = 20;
+
+
+if ($age>=18)
+{
+    echo 'vous êtes majeur';
+}
+else 
+{
+    echo 'vous êtes mineur';
+}
+
+?>
+</body>
+</html>
---
+++ CynthiaBoulevard/php/partie2/exo2/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Exo1</title>
+</head>
+<body>
+<?php
+
+
+$isEasy = true;
+
+
+if ($isEasy = true)
+{
+    echo 'facile';
+}
+else 
+{
+    echo 'difficile';
+}
+
+
+
+// $isEasy = true;
+
+
+// if ($isEasy = false)
+// {
+    // echo 'difficile';
+// }
+// else 
+// {
+    // echo 'facile';
+// }
+
+
+
+?>
+</body>
+</html>
